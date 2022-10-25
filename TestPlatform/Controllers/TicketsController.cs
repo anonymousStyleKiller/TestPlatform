93212e59fefb8808f19dd99e8d41a5382e0c7835
Anton <anonymousStyleKiller@gmail.com>
2022-10-25 22:46:15 +0300

    feature(Server): Projects and Tickets controllers

---
+++ TestPlatform/Controllers/TicketsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestPlatform.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TicketsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult Get(string id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult Put()
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+        return Ok(id);
+    }
+}
